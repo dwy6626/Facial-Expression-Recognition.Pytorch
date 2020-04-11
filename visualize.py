7e9c31dbb27b7c2dfa50a20eaa338fd01e24d153
De-Wei Ye <DeWeiYe6626@gmail.com>
2020-04-11 23:54:09 +0800

    fix visualize bug, add git ignore

--- visualize.py
+++ visualize.py
@@ -17,6 +17,16 @@ from skimage.transform import resize
 from models import *
 
 cut_size = 44
+output_dir = images/results
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+weight_path = os.path.join('FER2013_VGG19', 'PrivateTest_model.t7')
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# -----------------
+# image pre-process
+# -----------------
 
 transform_test = transforms.Compose([
     transforms.TenCrop(cut_size),
@@ -36,10 +46,12 @@ img = np.concatenate((img, img, img), axis=2)
 img = Image.fromarray(img)
 inputs = transform_test(img)
 
-class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+# ----------
+# Evaluation
+# ----------
 
 net = VGG('VGG19')
-checkpoint = torch.load(os.path.join('FER2013_VGG19', 'PrivateTest_model.t7'))
+checkpoint = torch.load(weight_path)
 net.load_state_dict(checkpoint['net'])
 net.cuda()
 net.eval()
@@ -51,19 +63,22 @@ inputs = inputs.cuda()
 inputs = Variable(inputs, volatile=True)
 outputs = net(inputs)
 
-outputs_avg = outputs.view(ncrops, -1).mean(0)  # avg over crops
+# avg over crops
+outputs_avg = outputs.view(ncrops, -1).mean(0)  
 
 score = F.softmax(outputs_avg)
 _, predicted = torch.max(outputs_avg.data, 0)
 
+# -------------
+# Visualization
+# -------------
+
 plt.rcParams['figure.figsize'] = (13.5,5.5)
-axes=plt.subplot(1, 3, 1)
+axes = plt.subplot(1, 3, 1)
 plt.imshow(raw_img)
 plt.xlabel('Input Image', fontsize=16)
 axes.set_xticks([])
 axes.set_yticks([])
-plt.tight_layout()
-
 
 plt.subplots_adjust(left=0.05, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
 
@@ -78,19 +93,15 @@ plt.xlabel(" Expression Category ",fontsize=16)
 plt.ylabel(" Classification Score ",fontsize=16)
 plt.xticks(ind, class_names, rotation=45, fontsize=14)
 
-axes=plt.subplot(1, 3, 3)
+axes = plt.subplot(1, 3, 3)
 emojis_img = io.imread('images/emojis/%s.png' % str(class_names[int(predicted.cpu().numpy())]))
 plt.imshow(emojis_img)
 plt.xlabel('Emoji Expression', fontsize=16)
 axes.set_xticks([])
 axes.set_yticks([])
 plt.tight_layout()
-# show emojis
 
-#plt.show()
-plt.savefig(os.path.join('images/results/l.png'))
+plt.savefig(os.path.join(output_dir, 'l.png'))
 plt.close()
 
 print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
-
-
